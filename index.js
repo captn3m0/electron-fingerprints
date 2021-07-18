f3b7beb6421d1bd6d1db132029ca359a30f4e618
Nemo <me@captnemo.in>
2021-07-18 17:18:41 +0530

    Move lookup tables to separate directory

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "linux-x64": require('./linux-x64'),
-  "linux-arm64": require('./linux-arm64'),
-  "darwin-x64": require('./darwin-x64'),
-  "darwin-arm64": require('./darwin-arm64'),
-  "win32-x64": require('./win32-x64'),
-  "win32-arm64": require('./win32-arm64'),
+  "linux-x64": require('./lookup/linux-x64'),
+  "linux-arm64": require('./lookup/linux-arm64'),
+  "darwin-x64": require('./lookup/darwin-x64'),
+  "darwin-arm64": require('./lookup/darwin-arm64'),
+  "win32-x64": require('./lookup/win32-x64'),
+  "win32-arm64": require('./lookup/win32-arm64'),
 }
